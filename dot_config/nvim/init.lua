4d8db2d874c800fa50c1c792684d0a5066c16015
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-02-24 01:38:54 +0000

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -524,7 +524,7 @@ suggestion = {
 
 -- F5 to toggle nerdtree
 -- vim.api.nvim_set_keymap('n', '<F5>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<F5>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 -- Run make with F9
 vim.api.nvim_set_keymap('n', '<F9>', ':w | terminal make<CR>:startinsert<CR>', { noremap = true, silent = true })
