e03db8dd22836b255fad180a17297d359838758b
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-02-25 02:05:03 +0000

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -535,9 +535,11 @@ vim.g.loaded_netrwPlugin = 1
 -- F5 to toggle filetree 
 vim.api.nvim_set_keymap('n', '<F5>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 require("nvim-tree").setup()
+-- F6 to fuzzy find file
+vim.api.nvim_set_keymap('n', '<F6>', '::Telescope find_files<CR>', { noremap = true, silent = true })
 
 -- Run make with F9
-vim.api.nvim_set_keymap('n', '<F9>', ':w | terminal make<CR>:startinsert<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ':w | vertical terminal make<CR>:startinsert<CR>', { noremap = true, silent = true })
 
 -- neovide config
 if vim.fn.exists("g:neovide") then
