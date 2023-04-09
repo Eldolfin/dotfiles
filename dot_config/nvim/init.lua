339d0198892f0a9fffc3a3fdefa36a3bff0eb201
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-04-09 16:51:03 +0100

    Update .config/alacritty/alacritty.yml Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/nvim-dap.lua Update .config/zellij/config.kdl Update .tmux.conf Update install-packages.sh

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,13 +1,19 @@
+-- Vimspector keybinds
+vim.g.vimspector_enable_mappings = "HUMAN"
+
+-- don't continue comments when pressing enters
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- nvim cmp <-> autopairs
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 -- enable folding
 local opt = vim.opt
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevelstart = 99
-
--- nvim cmp <-> autopairs
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,23 +1,51 @@
 return {
-  "rcarriga/nvim-dap-ui",
-  dependencies = { "mfussenegger/nvim-dap" },
+  "mfussenegger/nvim-dap",
   config = function()
-    local dap, dapui = require("dap"), require("dapui")
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-    vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
-    vim.keymap.set("n", "<F5>", require("dap").continue)
-    vim.keymap.set("n", "<F10>", require("dap").step_over)
-    vim.keymap.set("n", "<F11>", require("dap").step_into)
-    vim.keymap.set("n", "<F12>", require("dap").step_out)
-    vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint)
+    local dap = require("dap")
+    vim.fn.sign_define("DapBreakpoint", {
+      text = "",
+      texthl = "DiagnosticSignError",
+      linehl = "",
+      numhl = "",
+    })
+    vim.fn.sign_define("DapBreakpointRejected", {
+      text = "",
+      texthl = "DiagnosticSignError",
+      linehl = "",
+      numhl = "",
+    })
+    vim.fn.sign_define("DapStopped", {
+      text = "",
+      texthl = "DiagnosticSignWarn",
+      linehl = "Visual",
+      numhl = "DiagnosticSignWarn",
+    })
+
+    dap.set_log_level("info")
+
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "/home/oscar/.local/share/nvim/lazy/nvim-dap/debuggers/codelldb/adapter/codelldb",
+        args = { "--port", "${port}" },
+      },
+    }
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      },
+    }
+    -- use the same config for C and rust too
+    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.rust = dap.configurations.cpp
   end,
 }
