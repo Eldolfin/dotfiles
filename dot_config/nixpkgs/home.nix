1c7728c2e367ca58acbbc4a206d6545ae6c9a88c
Eldolfin <oscar.le-dauphin@epita.fr>
2023-05-23 17:59:50 +0200

    Revert "Update .config/nixpkgs/home.nix"
    This reverts commit b2d519cadf99d26062d311ee3b83af2378758aa1.

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -2,20 +2,8 @@
 
 let
   unstable = import <unstable> { };
-
-  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
-  hyprland = (import flake-compat {
-    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
-  }).defaultNix;
 in
 {
-  imports = [
-    hyprland.homeManagerModules.default
-  ];
-
-  wayland.windowManager.hyprland.enable = true;
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "oscar";
