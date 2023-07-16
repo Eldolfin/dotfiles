6445cef384ace9c37e72b8eddea66a5021e11c81
Eldolfin <oscar.le-dauphin@epita.fr>
2023-07-16 20:35:37 +0200

    update

--- dot_config/i3/executable_update_background.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-files=(~/.backgrounds/*)
-WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-# WALLPAPER2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-feh -B white --bg-fill $WALLPAPER #--bg-fill $WALLPAPER2
