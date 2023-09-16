93abae13da848c8f72affa7379d3e378bb9a5c4a
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-09-16 18:10:51 +0200

    Add .config/nixpkgs/basePackages.nix Add .config/nixpkgs/personalPackages.nix

---
+++ dot_config/nixpkgs/basePackages.nix
@@ -0,0 +1,71 @@
+{ pkgs, ... }:
+
+
+let
+  unstable = import <unstable> { };
+in
+{
+home.packages = with pkgs; [
+    # cli tools
+    tealdeer
+    skim
+    jq
+    joshuto
+    dive
+    ncdu
+    calcurse
+    git-lfs
+    exa
+    fd
+    fzf
+    btop
+    zsh
+    neofetch
+    chezmoi
+    zoxide
+    zellij
+    lazygit
+    bat
+    ripgrep
+
+    # graphical programs
+    rofi
+    rofi-bluetooth
+    rofi-power-menu
+    copyq
+    flameshot
+    emote
+    dunst
+    redshift
+
+    # Language servers
+    sumneko-lua-language-server
+    mypy
+    python310Packages.python-lsp-server
+
+
+    # libraries
+    libnotify
+    boost
+    armadillo
+    pkgconfig
+    stdenv.cc.cc.lib
+    luajit
+    stylua
+    vscodium
+
+    # python packages
+    (python310.withPackages (ps: with ps; [ 
+    ruff-lsp
+    pynvim
+    pip
+    ]))
+
+    # font
+    meslo-lgs-nf
+    fusuma
+  ] ++ (with unstable; [
+    # cli
+    neovim
+  ]);
+  }
