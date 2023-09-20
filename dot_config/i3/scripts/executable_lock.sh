bfacbc1b475c5a4d4e9430c865b461e927e961b4
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-09-20 18:56:05 +0200

    Update .config/i3/scripts/lock.sh

--- dot_config/i3/scripts/executable_lock.sh
+++ dot_config/i3/scripts/executable_lock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-i3lock -c '#000000' --ignore-empty-password --show-failed-attempts --nofork
+i3lock -c '#000000' --ignore-empty-password -i ~/afs/.confs/Bsodwindows10.png --nofork
