3905fb03359fbc6f9f7d61b8ca41a183f0b10cf2
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-10-10 22:06:47 +0200

    Add .config/fish/functions/exo_piscine.fish

---
+++ dot_config/private_fish/functions/exo_piscine.fish
@@ -0,0 +1,4 @@
+function exo_piscine --wraps=/home/oscar.le-dauphin/afs/Boilerplate/C/exo_piscine.sh --description 'alias exo_piscine=/home/oscar.le-dauphin/afs/Boilerplate/C/exo_piscine.sh'
+  /home/oscar.le-dauphin/afs/Boilerplate/C/exo_piscine.sh $argv
+        
+end
