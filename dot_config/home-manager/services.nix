02a2ce61125a67f1751e78410c2fbb7106369f45
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-10-16 12:36:54 +0200

    Update .config/home-manager/home.nix Add .config/home-manager/services.nix

---
+++ dot_config/home-manager/services.nix
@@ -0,0 +1,28 @@
+{
+  services = {
+    flameshot.enable = true;
+    # bluetooth media control
+    mpris-proxy.enable = true;
+    picom = {
+      enable = true;
+      shadow = true;
+      inactiveOpacity = 0.97;
+      opacityRules = [ "100:class_g = 'i3lock'" ];
+      fade = true;
+      fadeSteps = [ 0.2 0.2 ];
+      backend = "glx";
+      settings = {
+        "corner-radius" = 10;
+        "rounded-corners-exclude" = [
+          "window_type = 'dock'"
+          "window_type = 'desktop'"
+        ];
+      };
+    };
+
+    gnome-keyring = {
+      enable = true;
+      components = [ "pkcs11" "secrets" "ssh" ];
+    };
+  };
+}
