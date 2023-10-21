9d1865fce16be12cb9c55193f30e5ee40de47ef4
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-10-21 23:25:34 +0200

    Add .config/fish/functions/black.fish Add .config/fish/functions/la.fish Add .config/fish/functions/nix-shell.fish Add .config/fish/functions/svi.fish Add .config/fish/functions/vimdiff.fish

---
+++ dot_config/private_fish/functions/black.fish
@@ -0,0 +1,4 @@
+function black --wraps='xsetroot -solid "#000000"' --description 'alias black=xsetroot -solid "#000000"'
+  xsetroot -solid "#000000" $argv
+        
+end
---
+++ dot_config/private_fish/functions/svi.fish
@@ -0,0 +1,4 @@
+function svi --wraps='sudo -E -s nvim' --description 'alias svi=sudo -E -s nvim'
+  sudo -E -s nvim $argv
+        
+end
