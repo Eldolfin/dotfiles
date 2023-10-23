e70d865f099120005fe430910697a923ab6fb081
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-10-23 19:01:14 +0200

    Update .config/polybar/launch.sh Update .config/fish/fish_variables

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
 # Launch bar1 and bar2
-MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-MONITORS=$MONITORS polybar top &
-MONITOR=$MONITORS polybar bottom;
+# polybar bar 2>&1 | tee -a /tmp/polybar.log &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar bar 2>&1 | tee -a /tmp/polybar.log &
+done
+disown
 
 echo "Bars launched..."
