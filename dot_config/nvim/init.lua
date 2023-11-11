ee64cfdaa84b20aa557a793f8d4a36f4668caeb6
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2023-11-12 00:25:20 +0100

    Update .config/nvim/init.lua Update .config/nvim/lua/config/keymaps.lua Update .config/fish/fish_variables

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,71 +1,10 @@
--- Vimspector keybinds
-vim.g.vimspector_enable_mappings = "HUMAN"
-
--- don't continue comments when pressing enters
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- nvim cmp <-> autopairs
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
--- enable folding
-local opt = vim.opt
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldlevelstart = 99
+require("config.lsp_shit")
+require("config.after")
 
 -- set python interpreter path (nix)
 vim.g.python3_host_prog = "/home/oscar/.nix-profile/bin/python3"
 
--- manual lsp installs, because nix
-local nvim_lsp = require("lspconfig")
-nvim_lsp['pylsp'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/pylsp' },
-}
-
-nvim_lsp['ruff_lsp'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/ruff-lsp' },
-}
-
-nvim_lsp['lua_ls'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/lua-language-server' },
-}
-
-nvim_lsp['clangd'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/clangd' },
-}
-
-nvim_lsp['rust_analyzer'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/rust-analyzer' },
-}
-
-nvim_lsp['rnix'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/rnix-lsp' },
-}
-
-nvim_lsp['omnisharp'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/OmniSharp', '--languageserver', '--hostPID', tostring(vim.fn.getpid()) },
-}
-
-nvim_lsp['pyright'].setup {
-    cmd = { '/home/oscar/.nix-profile/bin/pyright-langserver', '--stdio' },
-}
-
-
-if vim.g.neovide then
-    vim.g.neovide_scale_factor = 1.0
-    local change_scale_factor = function(delta)
-        vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-    end
-    vim.keymap.set("n", "<C-=>", function()
-        change_scale_factor(1.10)
-    end)
-    vim.keymap.set("n", "<C-->", function()
-        change_scale_factor(1 / 1.10)
-    end)
-    vim.o.guifont = "MesloLGS NF:h14" -- text below applies for VimScript
-end
+-- don't continue comments when pressing enters
+vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,14 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- greatest remap ever (paste without replacing yank)
+vim.keymap.set("x", "<leader>p", '"_dP')
+
 local Job = require("plenary.job")
 
 -- async
 local function custom_make()
-  vim.api.nvim_command('write')
+  vim.api.nvim_command("write")
   Job:new({
     command = "alacritty",
-    args = { "-o", "font.size=10","-e", "bash", "-c", "i3-msg floating enable; make check || sleep 1d" },
+    args = { "-o", "font.size=10", "-e", "bash", "-c", "i3-msg floating enable; make check || sleep 1d" },
   }):start()
 end
 
@@ -23,9 +26,16 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true, callback = custom_make }
 )
 
-require("which-key").register({
-  c = {
-    p = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Projects symbols" },
-    o = { "<cmd>Telescope lsp_document_symbols<cr>", "File symbols" },
-  },
-}, { prefix = "<leader>" })
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.10)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.10)
+  end)
+  vim.o.guifont = "MesloLGS NF:h14" -- text below applies for VimScript
+end
