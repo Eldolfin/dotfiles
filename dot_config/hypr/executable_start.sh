1d577659709e11ad1335afb6886bcd69a8362f29
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-03-09 23:08:34 +0100

    Add .config/hypr/start.sh Update .config/hypr/hyprland.conf

---
+++ dot_config/hypr/executable_start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# init wallpaper daemon
+swww init &
+# set wallpaper
+files=(~/.backgrounds/*)
+WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+swww img $WALLPAPER &
+
+waybar &
+
+dunst &
+
+librewolf &
