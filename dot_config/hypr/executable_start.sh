c0e88be2528adf42dda78e2dc4733d073614e032
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-03-11 00:12:30 +0100

    Update .config/hypr/start.sh

--- dot_config/hypr/executable_start.sh
+++ dot_config/hypr/executable_start.sh
@@ -4,15 +4,16 @@ set -xe
 
 # init wallpaper daemon
 swww init &
-# set wallpaper
-files=(~/.backgrounds/*)
-WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-swww img $WALLPAPER &
 
 waybar &
 # ags -c ~/.config/ags/config.js &
 
 dunst &
+
+# set wallpaper
+files=(~/.backgrounds/*)
+WALLPAPER=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+swww img $WALLPAPER &
 
 copyq --start-server &
 
