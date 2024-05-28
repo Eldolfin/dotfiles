be16d08f018802e5d92784720c88c57cff55131f
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-05-28 23:26:18 +0200

    Update .config/home-manager/pkgs/terminal.nix

--- dot_config/home-manager/pkgs/terminal.nix
+++ dot_config/home-manager/pkgs/terminal.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 {
-    programs.alacritty.enable = true;
+    programs.alacritty = {
+        enable = true;
+        settings = {
+            font = {
+                normal = {
+                    family = "JetBrainsMono Nerd Font";
+                    size = 16;
+                };
+            };
+        };
+    };
     # programs.wezterm.enable = true;
   home.packages = with pkgs; [
     # kitty
