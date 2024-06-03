881a133f1662905183eb86c6f978d4b33bdf7b8a
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-04 00:15:09 +0200

    Update .config/home-manager/personalPackages.nix

--- dot_config/home-manager/personalPackages.nix
+++ dot_config/home-manager/personalPackages.nix
@@ -17,106 +17,105 @@
   programs.emacs.enable = true;
   programs.librewolf.enable = true;
 
-  home.packages = with pkgs;
-    [
-      # graphical programs
-      prismlauncher
-      virt-manager
-      gparted
-      signal-desktop
-      ckb-next
-      corectrl
-      godot_4
-      gimp
-      mumble
-      rustdesk
-      ungoogled-chromium
-      barrier
-      sunshine
-      kdeconnect
-      insomnia
-      # discord
-      armcord
-      freetube
-      bitwarden
-      mattermost-desktop
-      nextcloud-client
+  home.packages = with pkgs; [
+    # graphical programs
+    prismlauncher
+    virt-manager
+    gparted
+    signal-desktop
+    ckb-next
+    corectrl
+    godot_4
+    gimp
+    mumble
+    rustdesk
+    ungoogled-chromium
+    barrier
+    sunshine
+    kdeconnect
+    insomnia
+    # discord
+    armcord
+    discord
+    freetube
+    bitwarden
+    mattermost-desktop
+    nextcloud-client
 
-      # libraries
-      glfw
-      libllvm
-      gnumake
-      libnotify
-      dotnet-sdk_7
-      omnisharp-roslyn
-      # clang
-      boost
-      armadillo
-      pre-commit
-      stdenv.cc.cc.lib
-      luajit
-      wine
-      stylua
-      gpp
-      gamemode
-      mangohud
-      deno
-      winetricks
-      pyright
-      xorg.libXtst.out
-      mold
-      obs-studio
-      handbrake
-      # graalvm-ce
-      tree-sitter
-      luajitPackages.luarocks
-      parallel
-      black
-      nix-tree
-      libguestfs
-      kdocker
-      gitea-actions-runner
+    # libraries
+    glfw
+    libllvm
+    gnumake
+    libnotify
+    dotnet-sdk_7
+    omnisharp-roslyn
+    # clang
+    boost
+    armadillo
+    pre-commit
+    stdenv.cc.cc.lib
+    luajit
+    wine
+    stylua
+    gpp
+    gamemode
+    mangohud
+    deno
+    winetricks
+    pyright
+    xorg.libXtst.out
+    mold
+    obs-studio
+    handbrake
+    # graalvm-ce
+    tree-sitter
+    luajitPackages.luarocks
+    parallel
+    black
+    nix-tree
+    libguestfs
+    kdocker
+    gitea-actions-runner
 
-      # font
-      source-code-pro
-      meslo-lgs-nf
+    # font
+    source-code-pro
+    meslo-lgs-nf
 
-      man-pages
-      man-pages-posix
+    man-pages
+    man-pages-posix
 
-      fusuma
+    fusuma
 
-      # libs
-      nodejs
-      openssl
-      dnsmasq
-      criterion
-      gtest
-      gcovr
+    # libs
+    nodejs
+    openssl
+    dnsmasq
+    criterion
+    gtest
+    gcovr
 
-      # gui apps
-      flameshot
-      emote
-      syncthing
-      dunst
-      thunderbird
-      redshift
-      ddccontrol
-      ddcutil
-      ddcui
-      qbittorrent
-      libreoffice
-      virt-manager
-      mullvad-vpn
-      noisetorch
-      opensnitch-ui
-      xorg.xbacklight
-      cpufrequtils
-      pkg-config
-      whatsapp-for-linux
+    # gui apps
+    flameshot
+    emote
+    syncthing
+    dunst
+    thunderbird
+    redshift
+    ddccontrol
+    ddcutil
+    ddcui
+    qbittorrent
+    libreoffice
+    virt-manager
+    mullvad-vpn
+    noisetorch
+    opensnitch-ui
+    xorg.xbacklight
+    cpufrequtils
+    pkg-config
+    whatsapp-for-linux
 
-
-      # graphical programs
-      blender
-    ];
+    # graphical programs
+    blender
+  ];
 }
