2f1efa19f1141812353f708d98bb58ec8b8d6e26
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-06-12 07:30:50 +0200

    Update bin/scripts/systemswitch.sh

--- bin/scripts/executable_systemswitch.sh
+++ bin/scripts/executable_systemswitch.sh
@@ -7,7 +7,7 @@ trap 'notify-send "System switch failed 😢"' ERR
 pushd /etc/nixos/
 $EDITOR .
 echo "Rebuilding system..."
-git add . && git commit -m "System switch"
+git add . && git commit -m "System switch" || true
 nh os switch /etc/nixos
 git push
 popd
