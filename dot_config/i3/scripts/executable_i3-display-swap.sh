0aeff5522738eed7376298bfb6f3187ef84570f0
Eldofin <90446228+Eldolfin@users.noreply.github.com>
2024-07-12 02:44:55 +0200

    Update .config/i3/scripts/i3-display-swap.sh Update .gitconfig

--- dot_config/i3/scripts/executable_i3-display-swap.sh
+++ dot_config/i3/scripts/executable_i3-display-swap.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
-# requires jq
 
-DISPLAY_CONFIG=($(i3-msg -t get_outputs | jq -r '.[]|"\(.name):\(.current_workspace)"'))
+if ! command -v jq &>/dev/null; then
+	echo "jq could not be found"
+	exit 1
+fi
 
-for ROW in "${DISPLAY_CONFIG[@]}"; do
-	IFS=':'
-	read -ra CONFIG <<<"${ROW}"
-	if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
-		echo "moving ${CONFIG[1]} right..."
-		i3-msg -- workspace --no-auto-back-and-forth "${CONFIG[1]}"
-		i3-msg -- move workspace to output right
-	fi
+# Swaps workspaces between two displays, focus stays on the current active display
+
+DISPLAY_CONFIG="$(i3-msg -t get_outputs | jq -r '.[]|select(.active == true) |"\(.current_workspace)"')"
+ACTIVE_DISPLAY="$(i3-msg -t get_workspaces | jq -r '.[]|select(.focused == true) | "\(.output)"')"
+IFS=$'\n'
+for ROW in ${DISPLAY_CONFIG}; do
+	i3-msg -- workspace --no-auto-back-and-forth "$ROW"
+	i3-msg -- move workspace to output next
 done
+sleep 0.15
+i3-msg -- focus output "${ACTIVE_DISPLAY}"
